48f267cdcbd1e9719d0189ab98bef5b2508208c1
Pavel Nebeský <pavelnebesky@Pavel-iMac.local>
2023-06-27 16:21:56 +0200

    initial commit

---
+++ src/test/java/eu/nebesky/brain/BrainApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.nebesky.brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
